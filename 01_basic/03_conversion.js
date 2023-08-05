eef0ef18e32d80ad1624082afeb25504c72a8227
Suddukumar <138306342+Suddukumar@users.noreply.github.com>
2023-08-05 07:57:16 +0000

    full

--- 01_basic/03_conversion.js
+++ 01_basic/03_conversion.js
@@ -15,12 +15,51 @@ let valueInNumber =Number(score)
 let isLogedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLogedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 // 1=> true 0=> false
 // ""=> false
 // "suddu"=> true 
 
 let sumNumber= 33
 let stringNumber= String(sumNumber)
-console.log(stringNumber);
-console.log( typeof stringNumber);
+// console.log(stringNumber);
+// console.log( typeof stringNumber);
+
+// *************************** opeation *********************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2); additon
+// console.log(2-2); substration
+// console.log(2*2); multiplication
+// console.log(2**2); square
+// console.log(2/2); division
+// console.log(2%2); module/remainer
+
+let str1="hello"
+let str2 =" suddu"
+
+let str3 = str1+str2
+
+// console.log(str3);
+// console.log("1" +2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+
+// console.log((3 + 4) * 5 % 3);
+
+// console.log(+true); // output=>1
+// console.log(+""); // output=>0
+ 
+let num1,num2,num3
+num1=num2=num3= 2+2
+
+
+let gameCounter=100
+// gameCounter++; post fix
+++gameCounter // prefix
+console.log(gameCounter);
