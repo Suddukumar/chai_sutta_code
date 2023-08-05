98dd4239dbaf12899ed1493ea876b0ecf1c4588a
Suddukumar <138306342+Suddukumar@users.noreply.github.com>
2023-08-05 07:41:00 +0000

    conversion

---
+++ 01_basic/03_conversion.js
@@ -0,0 +1,26 @@
+let score =undefined
+//  score ='33abc'
+
+// console.log(typeof score)
+// console.log(typeof (score));
+
+let valueInNumber =Number(score)
+// console.log( typeof valueInNumber);
+// console.log( valueInNumber);
+
+// "33 "=> 33
+// "33abc"=> NaN
+// true=> 1; false=>0
+
+let isLogedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLogedIn)
+console.log(booleanIsLoggedIn);
+// 1=> true 0=> false
+// ""=> false
+// "suddu"=> true 
+
+let sumNumber= 33
+let stringNumber= String(sumNumber)
+console.log(stringNumber);
+console.log( typeof stringNumber);
