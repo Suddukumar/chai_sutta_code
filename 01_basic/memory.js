24028f7e418f56147a38391de32f3540d026e49d
Suddukumar <138306342+Suddukumar@users.noreply.github.com>
2023-08-05 08:58:18 +0000

    done

---
+++ 01_basic/memory.js
@@ -0,0 +1,19 @@
+// ********************memories*********
+// stack(primitive),heap(non-premitive)
+
+let myYoutubename="suddu.com"
+
+let anothername=myYoutubename
+anothername="chaiaursutta"
+console.log(myYoutubename);
+console.log(anothername);
+
+let user={
+    email:"user@gmail.com",
+    upi:"user@ybl"
+}
+let user1 =user
+user1.email="suddu@123.com"
+
+console.log(user.email);
+console.log(user1.email);
